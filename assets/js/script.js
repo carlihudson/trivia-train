9e74d5d2b1836ce55edfc27a5554f3807d0176f8
Carli Hudson <carlihudson@Carlis-MacBook-Pro.local>
2022-11-23 22:17:15 -0800

    added trivia api functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,157 @@
-var letsPlayBtn = document.getElementById("lets-play")
-var landingPage = document.getElementById("landing")
-var questionPage = document.getElementById("question")
-// slide 1 notes -- 
-    // query selector for "Pick a Category" textbox (Carli 1)
-        $( function() {
-            var categories = [
-                "Art & Literature",
-                "Language",
-                "Science & Nature",
-                "General",
-                "Food & Drink",
-                "People & Places",
-                "Geography",
-                "History & Holidays",
-                "Entertainment",
-                "Toys & Games",
-                "Music",
-                "Math",
-                "Religion & Mythology",
-                "Sports & Leisure"
+var searchForm = $('#search-form')
+var searchBar = $('#search-bar')
+var letsPlayBtn = $("#lets-play")
+var landingPage = $("#landing")
+var questionPage = $("#question")
+var selectedCategory = $('#selected-category')
+var generatedQuestion = $('#generated-question')
+var revealAnswer = $("#reveal-answer")
+var answer = $("#answer")
+var gifContainer = $("#gif-container")
+var navButtons = $("#nav-buttons")
 
-            ];
-            $( "#search-bar" ).autocomplete({
-                source: categories
+// autocomplete function
+$( function() {
+    var categories = [
+        "Art & Literature",
+        "Language",
+        "Science & Nature",
+        "General",
+        "Food & Drink",
+        "People & Places",
+        "Geography",
+        "History & Holidays",
+        "Entertainment",
+        "Toys & Games",
+        "Music",
+        "Math",
+        "Religion & Mythology",
+        "Sports & Leisure"
+
+        ];
+        $( "#search-bar" ).autocomplete({
+            source: categories
         });
     });
-    letsPlayBtn.addEventListener("click", function(){
-landingPage.classList.add("hidden")
-questionPage.classList.remove("hidden")
+
+var userChoice = searchBar.val() 
+console.log(userChoice)
+
+var apiCategories = [
+    "artliterature",
+    "language",
+    "sciencenature",
+    "general",
+    "fooddrink",
+    "peopleplaces",
+    "geography",
+    "historyholidays",
+    "entertainment",
+    "toysgames",
+    "music",
+    "mathematics",
+    "religionmythology",
+    "sportsleisure"
+]
+
+var humanCategoryToApiCategoryMap = {
+    "Art & Literature": "artliterature",
+    "Language": "language",
+    "Science & Nature": "sciencenature",
+    "General": "general",
+    "Food & Drink": "fooddrink",
+    "People & Places": "peopleplaces",
+    "Geography": "geography",
+    "History & Holidays": "historyholidays",
+    "Entertainment": "entertainment",
+    "Toys & Games": "toysgames",
+    "Music": "music",
+    "Math": "mathematics",
+    "Religion & Mythology": "religionmythology",
+    "Sports & Leisure": "sportsleisure"
+}
+
+// If you need all the keys (e.g. the human readable names)
+  // Then you can do this `var categories = Object.keys(humanCategoryToApiCategoryMap)`
+
+// If you need all the values (e.g. the api names)
+  // Then you can do this `var categories = Object.values(humanCategoryToApiCategoryMap)`
+
+var userChoice = ''
+
+searchForm.on('click', generateQuestion)
+
+var handleFormSubmit = function (event) {
+  event.preventDefault();
+
+  userChoice = searchBar.val() 
+  console.log(userChoice)
+
+  $(selectedCategory).text(userChoice)
+
+  if (!userChoice) {
+    // add the little red text saying you much
+    console.log('You must select from the given categories');
+    return;
+  }
+
+};
+
+letsPlayBtn.on('click', handleFormSubmit);
+
+function generateQuestion() {
+    for(var i = 0; i < apiCategories.length; i++){
+        if(apiCategories[i].match(userChoice))
+        fetchCategory(apiCategories[i]) 
+            
+          }
+    }
+
+
+
+
+    
+function fetchCategory(apiCategories) {
+    var apiUrl = 'https://api.api-ninjas.com/v1/trivia?category=' + humanCategoryToApiCategoryMap[apiCategories]
+    var key = "D9pwikBVMORkil2A1lVj3A==sOYASMS2c1pikHtG"
+        
+    fetch(apiUrl, {
+        contentType: 'application/json',
+         headers: { 'X-Api-Key': key}
+    }).then(function(response) {
+        return response.json()
+    }) .then(function(data){
+        console.log(data)
+        
+    } )
+
+    
+
+}
+
+
+
+letsPlayBtn.on("click", function(){
+    landingPage.addClass("hidden")
+    questionPage.removeClass("hidden")
+    $(selectedCategory).text(userChoice)
+    fetchCategory(userChoice)
     })
+
+revealAnswer.on("click", function(){
+    answer.removeClass("hidden")
+    revealAnswer.addClass("hidden")
+    gifContainer.removeClass("hidden")
+    navButtons.removeClass("hidden")
+        })
+
+
+    
 
 // slide 2 notes -- (Josh 1)
     // fetch random facts API 
     // create array for generated question
         // query selector for generated questions
-    // query selector for "Reveal Answer Button"
-        // event listener for "Reveal Answer Button"
 
 // slide 3 notes --
     // fetch GIPHY API (Ameera 1)
@@ -47,5 +163,21 @@ questionPage.classList.remove("hidden")
     // query selector for "Play Again (New Category)" button (Mab 1)
         // event listener for "Play Again (New Category)" button (Mab 1)
     
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
